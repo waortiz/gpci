6bf80aa95689937e60ad82ce9c26d570ea7b7a1b
William <waortiz@hotmail.com>
2017-08-19 06:56:10 -0500

    Ajustes certificado.

--- src/java/co/edu/fnsp/gpci/controladores/ReporteController.java
+++ src/java/co/edu/fnsp/gpci/controladores/ReporteController.java
@@ -8,26 +8,31 @@ package co.edu.fnsp.gpci.controladores;
 import co.edu.fnsp.gpci.entidades.ReporteFuenteFinanciacionProyecto;
 import co.edu.fnsp.gpci.entidades.ReporteIntegranteProyecto;
 import co.edu.fnsp.gpci.entidades.ReporteProfesorProyecto;
-import co.edu.fnsp.gpci.entidades.ReporteProyecto;
 import co.edu.fnsp.gpci.entidades.ReporteProyectoInscrito;
 import co.edu.fnsp.gpci.entidades.ReporteProyectoPorGrupoInvestigacion;
+import co.edu.fnsp.gpci.entidades.TipoPersona;
 import co.edu.fnsp.gpci.entidadesVista.BusquedaProyectos;
-import co.edu.fnsp.gpci.entidadesVista.DatosIntegrantesProyectos;
+import co.edu.fnsp.gpci.servicios.IServicioMaestro;
 import co.edu.fnsp.gpci.servicios.IServicioReporte;
-import co.edu.fnsp.gpci.utilidades.Util;
-import com.google.gson.Gson;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 
 /**
  *
@@ -42,6 +47,9 @@ public class ReporteController {
     @Autowired
     private IServicioReporte servicioReporte;
 
+    @Autowired
+    private IServicioMaestro servicioMaestro;
+        
     /**
      *
      * @param model
@@ -70,7 +78,7 @@ public class ReporteController {
         return "reportes/proyectosPorGrupoInvestigacion";
     }
 
-     /**
+    /**
      *
      * @param model
      * @return
@@ -83,7 +91,7 @@ public class ReporteController {
 
         return "reportes/fuentesFinanciacionProyectos";
     }
-    
+
     @RequestMapping(value = "/proyectosEjecucionAtrasadosProfesor", method = RequestMethod.GET)
     public String mostrarProyectosEjecucionAtrasadosProfesor(Model model) {
 
@@ -93,8 +101,8 @@ public class ReporteController {
 
         return "reportes/proyectosEjecucionAtrasadosProfesor";
     }
-    
-   @RequestMapping(value = "/proyectosEjecucionAtrasadosProfesor", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/proyectosEjecucionAtrasadosProfesor", method = RequestMethod.POST)
     public String obtenerProyectosEjecucionAtrasadosProfesor(@ModelAttribute(value = "busquedaProyectos") BusquedaProyectos busquedaProyectos, Model model) {
 
         ArrayList<ReporteProfesorProyecto> proyectos = new ArrayList<>();
@@ -118,8 +126,8 @@ public class ReporteController {
 
         return "reportes/proyectosProfesor";
     }
-    
-   @RequestMapping(value = "/proyectosProfesor", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/proyectosProfesor", method = RequestMethod.POST)
     public String obtenerProyectosProfesor(@ModelAttribute(value = "busquedaProyectos") BusquedaProyectos busquedaProyectos, Model model) {
 
         ArrayList<ReporteProfesorProyecto> proyectos = new ArrayList<>();
@@ -134,7 +142,7 @@ public class ReporteController {
         return "reportes/proyectosProfesor";
     }
 
-     /**
+    /**
      *
      * @param model
      * @return
@@ -146,5 +154,54 @@ public class ReporteController {
         model.addAttribute("reporte", reporte);
 
         return "reportes/proyectosInscritos";
+    }
+
+     /**
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/generarCertificadoParticipanteProyecto", method = RequestMethod.GET)
+    public String mostrarGeneracionCertificadoParticipanteProyecto(Model model) {
+
+        List<TipoPersona> tiposPersona = servicioMaestro.obtenerTiposPersona();
+        model.addAttribute("tiposPersona", tiposPersona);
+        
+        return "reportes/certificadoParticipanteProyecto";
+    }
+    
+    @RequestMapping(value = "/generarCertificadoParticipanteProyecto/{numeroDocumento}", method = RequestMethod.GET)
+    public void generarCertificadoEstudiante(@PathVariable("numeroDocumento") long numeroDocumento, HttpServletResponse response) {
+        try {
+            String filePath = "F:\\Sample.doc";
+            XWPFDocument documento = new XWPFDocument(new FileInputStream(filePath));
+            documento = reemplazarTexto(documento, "NOMBRE_PARTICIPANTE", "MyValue1");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            documento.write(baos);
+            
+            response.setContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            response.setContentLength(baos.toByteArray().length);
+            response.setHeader("Content-Disposition", "attachment; filename=certificado");
+            FileCopyUtils.copy(baos.toByteArray(), response.getOutputStream());
+        } catch (Exception ex) {
+            logger.error(ex);
+        }
+    }
+
+    private static XWPFDocument reemplazarTexto(XWPFDocument documento, String textoEncontrar, String textoReemplazar) {
+        for (XWPFParagraph parrafo : documento.getParagraphs()) {
+            List<XWPFRun> runs = parrafo.getRuns();
+            if (runs != null) {
+                for (XWPFRun run : runs) {
+                    String text = run.getText(0);
+                    if (text != null && text.contains(textoEncontrar)) {
+                        text = text.replace(textoEncontrar, textoReemplazar);
+                        run.setText(text, 0);
+                    }
+                }
+            }
+        }
+
+        return documento;
     }
 }
